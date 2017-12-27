2d7ddb21576ad1750d83d529d49080e17b93aa0c
kita83 <kita83@gmail.com>
2017-12-27 22:40:34 +0900

    テストファイル作成

---
+++ feed/tests/test_views.py
@@ -0,0 +1,10 @@
+"""feedアプリのViewテスト"""
+from django.test import TestCase
+
+
+class FeedViewTest(TestCase):
+    def test_index(self):
+        """
+        index画面のアクセスができているかテストする
+        """
+        pass
