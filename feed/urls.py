860aec442beffa1e2495b3afec7e43c026023c56
kita83 <kita83@gmail.com>
2017-12-30 22:40:45 +0900

    FBV > CBV

--- feed/urls.py
+++ feed/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'feed'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('ch/<int:ch_cd>/', views.ChannelDetailView.as_view(), name='ch_detail'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,46 +1,71 @@
 import pprint
 import requests
 from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    """
-    トレンド一覧画面
+class IndexView(View):
+    template_name = 'feed/index.html'
 
-    iTunes APIからPodcastランキングを取得する
-    :param request:
-    :return: context:
-    """
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return render(request, 'feed/ch_detail.html', {'form': form})
 
-    # ジャンル"Software How-To"のPodcastランキング情報を取得
-    url = 'https://itunes.apple.com/jp/rss/topaudiopodcasts/genre=1480/json'
-    res = requests.get(url).json()
+        return render(request, self.template_name, {'form': form})
 
-    # 表示用データ抽出
-    feeds = []
-    for feed in res['feed']['entry']:
-        # エピソードタイトル
-        title = 'episode title'
-        # チャンネルタイトル
-        channel = feed['im:name']['label']
-        # 収録者
-        author = feed['im:artist']['label']
-        # リリース日
-        released_at = feed['im:releaseDate']['attributes']['label']
-        tmp = {
-            'title': title,
-            'channel': channel,
-            'author': author,
-            'released_at': released_at
+    def get(self, request):
+        # ジャンル"Software How-To"のPodcastランキング情報を取得
+        url = 'https://itunes.apple.com/jp/rss/topaudiopodcasts/genre=1480/json'
+        res = requests.get(url).json()
+
+        # 表示用データ抽出
+        feeds = []
+        for feed in res['feed']['entry']:
+            # エピソードタイトル
+            title = 'episode title'
+            # チャンネルタイトル
+            channel = feed['im:name']['label']
+            # 収録者
+            author = feed['im:artist']['label']
+            # リリース日
+            released_at = feed['im:releaseDate']['attributes']['label']
+            tmp = {
+                'title': title,
+                'channel': channel,
+                'author': author,
+                'released_at': released_at
+            }
+            feeds.append(tmp)
+
+        # debug
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(feeds)
+
+        context = {
+            'feeds': feeds
         }
-        feeds.append(tmp)
 
-    # debug
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(feeds)
+        return render(request, self.template_name, context=context)
 
-    context = {
-        'feeds': feeds
-    }
 
-    return render(request, 'feed/index.html', context=context)
+class ChannelDetailView(View):
+    template_name = 'feed/ch_detail.html'
+
+    def get(self, request):
+        """
+        チャンネル詳細画面
+
+        指定された登録チャンネルの最新エピソードを表示する
+        """
+        new = []
+        tmp = {
+            'title': 'ep0001',
+            'author': 'chris',
+            'released_at': '2017年12月30日'
+        }
+        new.append(tmp)
+        context = {
+            'new_list': new
+        }
+        return render(request, self.template_name, context=context)
