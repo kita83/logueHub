db2147b91a1a4fe98d37a494b8b558e507425a63
kita83 <kita83@gmail.com>
2018-01-20 18:48:41 +0900

    ModelのPKをuuidに変更

--- feed/urls.py
+++ feed/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('all/', views.EpisodeAllView.as_view(), name='all'),
     path('like_list/', views.LikeListView.as_view(), name='like_list'),
     path('entry/', views.entry, name='entry'),
-    path('ch/<int:pk>/detail/', views.ChannelDetailView.as_view(), name='ch_detail'),
+    path('ch/<pk>/detail/', views.ChannelDetailView.as_view(), name='ch_detail'),
     path('settings/', views.SettingsView.as_view(), name='settings'),
 ]
