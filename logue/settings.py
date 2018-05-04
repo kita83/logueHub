25fcb4945729747f0dd0e034d99aea7b3856bbac
kita83 <kita83@gmail.com>
2018-05-04 13:45:57 +0900

    change STATICFILES_STRAGE

--- logue/settings.py
+++ logue/settings.py
@@ -251,7 +251,8 @@ DEFAULT_LOGGING = {
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 import dj_database_url
 db_from_env = dj_database_url.config()
