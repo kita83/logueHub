8687e92bb4cce30b1fdb89c9f5f7b4c960120417
kita83 <kita83@gmail.com>
2018-05-10 21:56:50 +0900

    delete media settings

--- logue/settings.py
+++ logue/settings.py
@@ -133,14 +133,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "media"),
+    # os.path.join(BASE_DIR, "media"),
 )
 
 AUTHENTICATION_BACKENDS = (
