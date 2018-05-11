b9238283384b3c2b1988ba263f136b1ee6b6fb13
kita83 <kita83@gmail.com>
2018-05-11 22:40:52 +0900

    change settings

--- logue/backends.py
+++ logue/backends.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-    location = 'media'  # /media というURLで配信
+    location = settings.AWS_LOCATION
     file_overwrite = False  # 同名ファイルは上書きせずに似た名前のファイルに
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = settings.AWS_PUBLIC_MEDIA_LOCATION
+    file_overwrite = False
--- logue/settings.py
+++ logue/settings.py
@@ -277,4 +277,5 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # メディアファイルの設定
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_PUBLIC_MEDIA_LOCATION = 'media/images'
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.PublicMediaStorage'
