9d2a23ccf779e5c1a4df601a76ecd49445e4d31d
kita83 <kita83@gmail.com>
2018-05-11 23:20:20 +0900

    change media_location

--- logue/settings.py
+++ logue/settings.py
@@ -277,5 +277,5 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # メディアファイルの設定
-AWS_PUBLIC_MEDIA_LOCATION = 'media/images'
+AWS_PUBLIC_MEDIA_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'logue.backends.PublicMediaStorage'
