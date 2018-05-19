25aa1c67efe4451733f052eb616f167668dc911e
kita83 <kita83@gmail.com>
2018-05-19 23:03:42 +0900

    change settings

--- logue/settings.py
+++ logue/settings.py
@@ -36,7 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 TEMPLATES = [
