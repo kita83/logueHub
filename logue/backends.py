4c49082654be564e7e8686e6bfb18976cd83dcc6
kita83 <kita83@gmail.com>
2018-06-25 22:03:26 +0900

    change MEDIA_URL

--- logue/backends.py
+++ logue/backends.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class MediaStorage(S3Boto3Storage):
+class StaticStorage(S3Boto3Storage):
     location = settings.AWS_LOCATION
     file_overwrite = False  # 同名ファイルは上書きせずに似た名前のファイルに
 
--- logue/settings.py
+++ logue/settings.py
@@ -125,7 +125,6 @@ else:
     # 静的ファイルの設定
     AWS_LOCATION = 'static'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
     # メディアファイルの設定
